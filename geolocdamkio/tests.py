f72ba0b01e24075191d6e240dc81e9534e870513
Wh1z95 <damkio12@gmail.com>
2019-06-18 00:11:38 +0200

    Add files via upload

--- geolocdamkio/tests.py
+++ geolocdamkio/tests.py
@@ -1,10 +1,29 @@
+from geolocdamkio import models
 import unittest
-from geolocdamkio.models import Localization
+from selenium import webdriver
 
 
 class LocalizationTestCase(unittest.TestCase):
+
+
     def setUp(self):
-        self.bialystok = Localization.objects.create(name='Park', location=(), address='Branickiego 5', city='Bialystok')
-        self.warsaw = Localization.objects.create(name='LotniskoModlin', location=(), address='Generała Wiktora Thommée 1a', city='Warsaw')
+        self.driver = webdriver.Firefox()
+
+
+        def testaddloc(self):
+            self.driver.get("http://localhost:8000/add/")
+            self.driver.find_element_by_id('name').send_keys("Magnus Carlsen")
+            self.driver.find_element_by_id('localization').send_keys("Norwegia")
+            self.driver.find_element_by_id('address').send_keys("sniezna 39")
+            self.driver.find_element_by_id('city').send_keys("oslo")
+            self.assertIn(("http://localhost:8000/", self.driver.current_url)
+
+
+        def tearDown(self):
+            self.driver.quit
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
